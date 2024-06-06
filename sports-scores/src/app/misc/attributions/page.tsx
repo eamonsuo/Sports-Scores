5af9e63c9026ce85510d18c4456137f292fb5d3e
Eamon <esuosaari@deloitte.com.au>
2024-06-06 16:40:34 +1000

    Removed reference to old image

--- sports-scores/src/app/misc/attributions/page.tsx
+++ sports-scores/src/app/misc/attributions/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import aflBall from "@/../public/noun-football-683692.svg"
+import aflBall from "@/../public/football.svg"
 
 export default async function Page() {
     return(<p><Image src={aflBall} width={30} height={30} alt="AFL ball" className="text-center"></Image><div>Icons made from <a href="https://www.onlinewebfonts.com/icon">svg icons</a>is licensed by CC BY 4.0</div></p>)
