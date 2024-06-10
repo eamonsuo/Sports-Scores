69ee45ec4009584960fcf479e627a1dc761161cd
Eamon <esuosaari@deloitte.com.au>
2024-06-10 15:37:28 +1000

    Added favicon and AFL API counter

--- sports-scores/src/app/sports/afl/page.tsx
+++ sports-scores/src/app/sports/afl/page.tsx
@@ -1,15 +1,18 @@
 import MatchSummaryList from "@/components/MatchSummaryList"
 import NavButtonGroup from "@/components/NavButtonGroup"
+import { fetchAflData } from "@/js/externalAPI";
 
 export default async function Page() {
   let buttonList: NavButtonGroupProps = [
     {label: "Matches", link: "/misc/coming"}, 
     {label: "Ladder", link: "/misc/coming"}];
 
+    const res = await fetchAflData("status");
+
   return (
   <div className="flex flex-col h-full">
       <NavButtonGroup buttons={buttonList}></NavButtonGroup>
       <MatchSummaryList></MatchSummaryList>
-      <div className="m-2">API Counter: </div>
+      <div className="m-2">API Counter: {res.response.requests.current} out of {res.response.requests.limit_day}</div>
   </div>);
 }
