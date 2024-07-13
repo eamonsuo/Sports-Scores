1351dde0ed2332615e2bbd14ce3ab30d0facb431
Eamon <esuosaari@deloitte.com.au>
2024-07-13 12:40:43 +1000

    Updated nrl & golf pages for better Ui and info

--- sports-scores/src/app/sports/golf/page.tsx
+++ sports-scores/src/app/sports/golf/page.tsx
@@ -4,16 +4,15 @@ import { redirect } from "next/navigation";
 
 export default async function Page() {
   return (
-    <>
-      <div className="flex h-full flex-col">
-        <Link href={`https://www.livgolf.com/leaderboard`} className="">
-          <Button>LIV Tour</Button>
-        </Link>
-        <iframe
-          src="https://www.google.com/search?igu=1&gws_rd=ssl&q=pga"
-          className="flex-1"
-        />
-      </div>
-    </>
+    <div className="flex h-full flex-col items-center">
+      <Button variant="destructive"> Custom Look Coming Soon</Button>
+      <Link href={`https://www.livgolf.com/leaderboard`}>
+        <Button>View LIV Tour (Redirects)</Button>
+      </Link>
+      <iframe
+        src="https://www.google.com/search?igu=1&gws_rd=ssl&q=pga"
+        className="w-full flex-1"
+      />
+    </div>
   );
 }
