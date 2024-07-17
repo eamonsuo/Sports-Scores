adfec0b179b2d739104c42c89658e1afd1cd9e4a
Eamon <esuosaari@deloitte.com.au>
2024-07-17 16:16:03 +1000

    Refactor Footer component

--- sports-scores/src/components/misc/Footer.tsx
+++ sports-scores/src/components/misc/Footer.tsx
@@ -1,6 +1,9 @@
 "use client";
 import Link from "next/link";
 import Image from "next/image";
+import clsx from "clsx";
+import { useState } from "react";
+import { SPORT } from "@/lib/constants";
 
 import cricketBall from "@/../public/cricket-ball-icon.svg";
 import aflBall from "@/../public/football.svg";
@@ -9,159 +12,90 @@ import nflBall from "@/../public/american-football.svg";
 import nrlBall from "@/../public/rugby-game-svgrepo-com.svg";
 import f1Helmet from "@/../public/f1-helmet-svgrepo-com.svg";
 import golfBall from "@/../public/golf-ball-with-dents-svgrepo-com.svg";
-import oly from "@/../public/The Paris 2024 Summer Olympics and Paralympics.svg";
-import { SPORT, TAILWINDS } from "@/lib/constants";
-import clsx from "clsx";
-import { useState } from "react";
+import oly from "@/../public/Olympic_Rings.svg";
+
+const footerLinks: {
+  sport: string;
+  link: string;
+  img: string;
+  altText: string;
+}[] = [
+  {
+    sport: "abc news",
+    link: "https://www.abc.net.au/news/sport",
+    img: abc,
+    altText: "ABC News",
+  },
+  {
+    sport: SPORT.OLYMPICS,
+    link: `/sports/${SPORT.OLYMPICS}`,
+    img: oly,
+    altText: "Olympics",
+  },
+  {
+    sport: SPORT.CRICKET,
+    link: `/sports/${SPORT.CRICKET}`,
+    img: cricketBall,
+    altText: "Cricket",
+  },
+  {
+    sport: SPORT.NRL,
+    link: `/sports/${SPORT.NRL}`,
+    img: nrlBall,
+    altText: "NRL",
+  },
+  {
+    sport: SPORT.AFL,
+    link: `/sports/${SPORT.AFL}#current-date`,
+    img: aflBall,
+    altText: "AFL",
+  },
+  {
+    sport: SPORT.NFL,
+    link: `/sports/${SPORT.NFL}/main/matches`,
+    img: nflBall,
+    altText: "NFL",
+  },
+  {
+    sport: SPORT.F1,
+    link: `/sports/${SPORT.F1}`,
+    img: f1Helmet,
+    altText: "F1",
+  },
+  {
+    sport: SPORT.GOLF,
+    link: `/sports/${SPORT.GOLF}`,
+    img: golfBall,
+    altText: "Golf",
+  },
+];
 
 export default function Footer() {
   const [curSport, setCurSport] = useState("");
   return (
     <footer className="hideScroll flex h-16 w-full flex-row place-items-center gap-2 overflow-auto border-t bg-gray-200 p-1">
-      <Link href="https://www.abc.net.au/news/sport">
-        <div className={clsx(TAILWINDS.FOOTER_SPORT)}>
-          <Image
-            src={abc}
-            width={30}
-            height={30}
-            alt="ABC Logo"
-            className="text-center"
-          ></Image>
-        </div>
-      </Link>
-      <Link
-        href={`/sports/${SPORT.OLYMPICS}`}
-        onClick={() => setCurSport(SPORT.OLYMPICS)}
-      >
-        <div
-          className={clsx(
-            TAILWINDS.FOOTER_SPORT,
-            curSport === SPORT.OLYMPICS ? "bg-gray-500" : "bg-gray-400",
-          )}
+      {footerLinks.map((item) => (
+        <Link
+          key={item.sport}
+          href={item.link}
+          onClick={() => setCurSport(item.sport)}
         >
-          <Image
-            src={oly}
-            width={50}
-            height={50}
-            alt="Wiki"
-            className="text-center"
-          ></Image>
-        </div>
-      </Link>
-      <Link
-        href={`/sports/${SPORT.CRICKET}`}
-        onClick={() => setCurSport(SPORT.CRICKET)}
-      >
-        <div
-          className={clsx(
-            TAILWINDS.FOOTER_SPORT,
-            curSport === SPORT.CRICKET ? "bg-gray-500" : "bg-gray-400",
-          )}
-        >
-          <Image
-            src={cricketBall}
-            width={30}
-            height={30}
-            alt="cricket ball"
-            className="text-center"
-          ></Image>
-        </div>
-      </Link>
-      <Link
-        href={`/sports/${SPORT.NRL}`}
-        onClick={() => setCurSport(SPORT.NRL)}
-      >
-        <div
-          className={clsx(
-            TAILWINDS.FOOTER_SPORT,
-            curSport === SPORT.NRL ? "bg-gray-500" : "bg-gray-400",
-          )}
-        >
-          <Image
-            src={nrlBall}
-            width={30}
-            height={30}
-            alt="NRL Ball"
-            className="text-center"
-          ></Image>
-        </div>
-      </Link>
-      <Link
-        href={`/sports/${SPORT.AFL}#currentDate`}
-        onClick={() => setCurSport(SPORT.AFL)}
-      >
-        <div
-          className={clsx(
-            TAILWINDS.FOOTER_SPORT,
-            curSport === SPORT.AFL ? "bg-gray-500" : "bg-gray-400",
-          )}
-        >
-          <Image
-            src={aflBall}
-            width={30}
-            height={30}
-            alt="AFL ball"
-            className="text-center"
-          ></Image>
-        </div>
-      </Link>
-      <Link
-        href={`/sports/${SPORT.NFL}/main/matches`}
-        onClick={() => setCurSport(SPORT.NFL)}
-      >
-        <div
-          className={clsx(
-            TAILWINDS.FOOTER_SPORT,
-            curSport === SPORT.NFL ? "bg-gray-500" : "bg-gray-400",
-          )}
-        >
-          <Image
-            src={nflBall}
-            width={30}
-            height={30}
-            alt="NFL ball"
-            className="text-center"
-          ></Image>
-        </div>
-      </Link>
-      <Link
-        href={`/sports/${SPORT.F1}#currentDate`}
-        onClick={() => setCurSport(SPORT.F1)}
-      >
-        <div
-          className={clsx(
-            TAILWINDS.FOOTER_SPORT,
-            curSport === SPORT.F1 ? "bg-gray-500" : "bg-gray-400",
-          )}
-        >
-          <Image
-            src={f1Helmet}
-            width={30}
-            height={30}
-            alt="F1 Helmet"
-            className="text-center"
-          ></Image>
-        </div>
-      </Link>
-      <Link
-        href={`/sports/${SPORT.GOLF}`}
-        onClick={() => setCurSport(SPORT.GOLF)}
-      >
-        <div
-          className={clsx(
-            TAILWINDS.FOOTER_SPORT,
-            curSport === SPORT.GOLF ? "bg-gray-500" : "bg-gray-400",
-          )}
-        >
-          <Image
-            src={golfBall}
-            width={30}
-            height={30}
-            alt="Golf Ball"
-            className="text-center"
-          ></Image>
-        </div>
-      </Link>
+          <div
+            className={clsx(
+              "flex size-11 place-content-center rounded-full",
+              curSport === item.sport ? "bg-gray-500" : "bg-gray-400",
+            )}
+          >
+            <Image
+              src={item.img}
+              width={30}
+              height={30}
+              alt={item.altText}
+              className="text-center"
+            ></Image>
+          </div>
+        </Link>
+      ))}
     </footer>
   );
 }
