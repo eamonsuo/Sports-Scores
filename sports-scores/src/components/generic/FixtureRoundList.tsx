7c208170b9f4eb37d8145a4039080cb23a823666
Eamon <esuosaari@deloitte.com.au>
2024-08-12 17:13:42 +1000

    Added a ref to ensure round buttons scroll into view when clicked/swiped

--- sports-scores/src/components/generic/FixtureRoundList.tsx
+++ sports-scores/src/components/generic/FixtureRoundList.tsx
@@ -14,13 +14,29 @@ export default function FixtureRoundList({
   curRound: string;
 }) {
   const [round, setRound] = useState(curRound); //need to put in parent so that it only gets set once. Need to change page structure?
-  const curBtn = useRef<HTMLButtonElement>(null);
+  const btnListRef = useRef<HTMLDivElement>(null); //Ref for the div which contains all round buttons
+  const initialBtn = useRef<HTMLButtonElement>(null); //Ref for btn of curRound
+  let roundLabels = [...new Set(data.map((item) => item.roundLabel ?? ""))];
 
   useEffect(() => {
-    curBtn.current?.scrollIntoView({ inline: "center", behavior: "smooth" });
+    //Ensure the curRound is scrolled into the centre of view on page load
+    initialBtn.current?.scrollIntoView({
+      inline: "center",
+      behavior: "smooth",
+    });
   }, []);
 
-  let roundLabels = [...new Set(data.map((item) => item.roundLabel ?? ""))];
+  //When called ensures the new round state is set and the related button is visible in view
+  function handleRoundClick(roundLabel: string) {
+    setRound(roundLabel);
+
+    const divNode = btnListRef.current;
+    const btnNode =
+      divNode?.querySelectorAll("button")[roundLabels.indexOf(roundLabel)];
+    btnNode?.scrollIntoView({
+      behavior: "smooth",
+    });
+  }
 
   // Swipe actions
   const MOVE_THRESHOLD = 100;
@@ -30,7 +46,6 @@ export default function FixtureRoundList({
 
   function touchStart(event: React.TouchEvent<HTMLDivElement>) {
     initialX = event.touches[0].pageX;
-    // console.log("initial", initialX, " ---  end", event);
   }
 
   function touchMove(event: React.TouchEvent<HTMLDivElement>) {
@@ -43,9 +58,9 @@ export default function FixtureRoundList({
 
     if (moveX < -MOVE_THRESHOLD) {
       curIndex + 1 !== roundLabels.length &&
-        setRound(roundLabels[curIndex + 1]);
+        handleRoundClick(roundLabels[curIndex + 1]);
     } else if (moveX > MOVE_THRESHOLD) {
-      curIndex !== 0 && setRound(roundLabels[curIndex - 1]);
+      curIndex !== 0 && handleRoundClick(roundLabels[curIndex - 1]);
     }
 
     moveX = 0;
@@ -53,12 +68,15 @@ export default function FixtureRoundList({
 
   return (
     <>
-      <div className="hideScroll mx-2 mb-2 flex gap-1 overflow-x-auto">
+      <div
+        ref={btnListRef}
+        className="hideScroll mx-2 mb-2 flex gap-1 overflow-x-auto"
+      >
         {roundLabels.map((item) => (
           <button
-            onClick={() => setRound(item)}
-            ref={item === curRound ? curBtn : null}
+            onClick={() => handleRoundClick(item)}
             key={item}
+            ref={item === curRound ? initialBtn : null} //Only use for initial page load
             className={clsx(
               "inline-flex items-center justify-center rounded-full px-2.5 py-0.5",
               item === round &&
