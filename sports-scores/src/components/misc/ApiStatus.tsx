bc4757cb0a6015689a16fa71a79fbd511f355f00
Eamon <esuosaari@deloitte.com.au>
2024-08-13 10:51:33 +1000

    fixed typing error from api status

--- sports-scores/src/components/misc/ApiStatus.tsx
+++ sports-scores/src/components/misc/ApiStatus.tsx
@@ -2,6 +2,6 @@ import { APISportsStatusDetails } from "@/types/misc";
 
 export default function APIStatus({ data }: { data: APISportsStatusDetails }) {
   return (
-    <div className="p-1 dark:bg-neutral-900 dark:text-neutral-400">{`API Calls: ${data.requests.current} out of ${data.requests.limit_day}`}</div>
+    <div className="p-1 dark:bg-neutral-900 dark:text-neutral-400">{`API Calls: ${data?.requests?.current} out of ${data?.requests?.limit_day}`}</div>
   );
 }
