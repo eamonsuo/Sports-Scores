991399b5e64854ac9c6673d27c69aaf541327398
Eamon <esuosaari@deloitte.com.au>
2025-07-07 17:54:54 +1000

    test

--- sports-scores/src/app/page.tsx
+++ sports-scores/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-between p-24 dark:text-neutral-400">
-      <p>Select a sport to get started</p>
+      <p>Select a sport to get started TEST</p>
     </div>
   );
 }
