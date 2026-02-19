b8b9896d026145aeb9cbb6852846b76ffc0d4cba
Eamon Suosaari <eamonsuosaari@gmail.com>
2026-02-19 17:12:51 +1000

    Added error pages

---
+++ sports-scores/src/app/sports/aussie-rules/error.tsx
@@ -0,0 +1,11 @@
+"use client"; // Error boundaries must be Client Components
+
+import ErrorPage from "@/components/misc-ui/ErrorPage";
+
+export default function Error({
+  error,
+}: {
+  error: Error & { digest?: string };
+}) {
+  return <ErrorPage externalUrl="https://www.google.com/search?q=afl" />;
+}
--- sports-scores/src/app/sports/rugby-league/error.tsx
+++ sports-scores/src/app/sports/rugby-league/error.tsx
@@ -1,17 +1,11 @@
 "use client"; // Error boundaries must be Client Components
 
 import ErrorPage from "@/components/misc-ui/ErrorPage";
-import { useEffect } from "react";
 
 export default function Error({
   error,
 }: {
   error: Error & { digest?: string };
 }) {
-  useEffect(() => {
-    // Log the error to an error reporting service
-    console.error(error);
-  }, [error]);
-
-  return <ErrorPage />;
+  return <ErrorPage externalUrl="https://www.google.com/search?q=nrl" />;
 }
