d0765c0660cbc35ebe3314b9d1cb7ebf00daa8d1
Eamon Suosaari <eamonsuosaari@gmail.com>
2026-02-21 17:53:18 +1000

    add headers to next

--- sports-scores/src/endpoints/sofascore.api.ts
+++ sports-scores/src/endpoints/sofascore.api.ts
@@ -35,6 +35,29 @@ export async function fetchNextEvents(
 ) {
   const rawEvents = await fetch(
     `${SOFASCOREBASEURL}/unique-tournament/${tournamentId}/season/${seasonId}/events/next/${pageNumber}`,
+    {
+      headers: {
+        accept: "*/*",
+        "accept-language": "en-US,en;q=0.9,en-AU;q=0.8",
+        baggage:
+          "sentry-environment=production,sentry-release=lDrhfvdVeEz69JNkkuy_a,sentry-public_key=d693747a6bb242d9bb9cf7069fb57988,sentry-trace_id=85ce8f88bbb1aa6a54f2c6d5496e0f4c",
+        "cache-control": "max-age=0",
+        "if-none-match": 'W/"372e8c00d6"',
+        "sec-ch-ua":
+          '"Not:A-Brand";v="99", "Google Chrome";v="145", "Chromium";v="145"',
+        "sec-ch-ua-mobile": "?1",
+        "sec-ch-ua-platform": '"iOS"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "sentry-trace": "85ce8f88bbb1aa6a54f2c6d5496e0f4c-8f78d03e080f2513",
+        "x-requested-with": "a64fa0",
+        cookie:
+          '_gcl_au=1.1.1046103336.1770438602; _ga=GA1.1.1249611337.1770438602; g_state={"i_l":0,"i_ll":1771648090807,"i_b":"WI2oFjPNv35lvhv63istjljEbaBSVEsZQ2B6MaqHBkA","i_e":{"enable_itp_optimization":0}}; _ga_HNQ9P9MGZR=GS2.1.s1771658994$o41$g1$t1771659006$j48$l0$h0',
+        Referer:
+          "https://www.sofascore.com/rugby/tournament/rugby-league/nrl/294",
+      },
+    },
   );
 
   if (!rawEvents.ok || errorCodesToNullify.includes(rawEvents.status)) {
