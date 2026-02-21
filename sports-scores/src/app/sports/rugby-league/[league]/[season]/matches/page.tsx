1f47f809072b466e7f310b2bd35224bd6867a980
Eamon Suosaari <eamonsuosaari@gmail.com>
2026-02-21 18:30:53 +1000

    client page

--- sports-scores/src/app/sports/rugby-league/[league]/[season]/matches/page.tsx
+++ sports-scores/src/app/sports/rugby-league/[league]/[season]/matches/page.tsx
@@ -1,12 +1,23 @@
+"use client";
 import FixtureRoundList from "@/components/all-sports/FixtureRoundList";
 import Placeholder from "@/components/misc-ui/Placeholder";
 import { rugbyLeagueMatches } from "@/services/rugby-league.service";
+import { RugbyLeagueFixturesPage } from "@/types/rugby-league";
+import { use, useEffect, useState } from "react";
 
-export default async function Page(props: {
+export default function Page(props: {
   params: Promise<{ league: string; season: string }>;
 }) {
-  const { league, season } = await props.params;
-  const pageData = await rugbyLeagueMatches(Number(league), Number(season));
+  const { league, season } = use(props.params);
+  const [pageData, setPageData] = useState<RugbyLeagueFixturesPage | null>(
+    null,
+  );
+
+  useEffect(() => {
+    rugbyLeagueMatches(Number(league), Number(season)).then((res) =>
+      setPageData(res),
+    );
+  }, [league, season]);
 
   if (pageData === null) {
     return <Placeholder>NO DATA</Placeholder>;
