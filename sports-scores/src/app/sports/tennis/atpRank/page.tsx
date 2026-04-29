b8221c0c2265763c69677128ac990f4725d69f2e
eamonsuo <eamonsuosaari@gmail.com>
2026-04-29 22:03:51 +1000

    build errors

--- sports-scores/src/app/sports/tennis/atpRank/page.tsx
+++ sports-scores/src/app/sports/tennis/atpRank/page.tsx
@@ -1,9 +1,10 @@
 import Placeholder from "@/components/misc-ui/Placeholder";
 import RankingsLeaderboard from "@/components/tennis/RankingsLeaderboard";
 import { tennisService } from "@/services/tennis.service";
+import { RankingList } from "@/types/tennis";
 
 export default async function Page() {
-  const pageData = await tennisService.TennisWorldRankings("atp");
+  const pageData = await tennisService.TennisWorldRankings(RankingList.ATP);
 
   if (pageData === null) {
     return <Placeholder>NO DATA</Placeholder>;
